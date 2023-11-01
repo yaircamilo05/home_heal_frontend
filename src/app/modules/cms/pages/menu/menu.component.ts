0d2710aa86ba4e02327f5d1e2843c25e3698997d
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2023-11-01 16:27:44 -0500

    first version crud Menu and create serviceMenu

--- src/app/modules/cms/pages/menu/menu.component.ts
+++ src/app/modules/cms/pages/menu/menu.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 import { Icons } from 'src/app/common/icon.modal';
 import { TitlesModal } from 'src/app/common/titles.modal';
 import { TypeModal } from 'src/app/common/type.modal';
+import { MenuGetModel } from 'src/app/models/menu.model';
 import { ModalMenusComponent } from 'src/app/modules/shared/components/modal-menus/modal-menus.component';
+import { MenuService } from 'src/app/services/menu.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-menu',
@@ -11,9 +14,24 @@ import { ModalMenusComponent } from 'src/app/modules/shared/components/modal-men
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+
+  menus: MenuGetModel[] = [];
+  
   constructor(
-    private dialog: Dialog
+    private dialog: Dialog,
+    private menuServices: MenuService,
   ) { }
+
+  ngOnInit(): void {
+    this.getAllMenus();
+  }
+
+  getAllMenus(){
+    this.menuServices.getAllMenus().subscribe(response => {
+      console.log(response);
+      this.menus = response.data;
+    })
+  }
 
   openModalCreateMenu(){
     let RefDialog = this.dialog.open(ModalMenusComponent, {
