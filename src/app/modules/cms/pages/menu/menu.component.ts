331908332430894800017f9fa7060916c53958f9
juandzapata <juan.zapata22871@ucaldas.edu.co>
2023-11-02 20:23:13 -0500

    Cambios para redondear bordes de la tabla (incompletos)

--- src/app/modules/cms/pages/menu/menu.component.ts
+++ src/app/modules/cms/pages/menu/menu.component.ts
@@ -1,65 +1,64 @@
 import { Dialog } from '@angular/cdk/dialog';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Icons } from 'src/app/common/icon.modal';
 import { TitlesModal } from 'src/app/common/titles.modal';
 import { TypeModal } from 'src/app/common/type.modal';
 import { MenuGetModel } from 'src/app/models/menu.model';
 import { ModalMenusComponent } from 'src/app/modules/shared/components/modal-menus/modal-menus.component';
 import { MenuService } from 'src/app/services/menu.service';
-import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
-
+export class MenuComponent implements OnInit {
   menus: MenuGetModel[] = [];
-  
+
   constructor(
     private dialog: Dialog,
-    private menuServices: MenuService,
-  ) { }
+    private menuServices: MenuService
+  ) {}
 
   ngOnInit(): void {
     this.getAllMenus();
   }
 
-  getAllMenus(){
-    this.menuServices.getAllMenus().subscribe(response => {
-      console.log(response);
-      this.menus = response.data;
-    })
+  getAllMenus() {
+    this.menuServices.getAllMenus().subscribe(
+      (response) => {
+        console.log(response); // Verifica si obtienes los datos correctamente
+        if (response && response.data) {
+          this.menus = response.data;
+        }
+      },
+      (error) => {
+        console.error('Error al obtener los menús:', error);
+      }
+    );
   }
 
-  openModalEditMenu(menu: MenuGetModel){
+  openModalEditMenu(menu: MenuGetModel) {
+    // Implementa esta función si necesitas editar menús
+  }
 
-  };
+  openModalDeleteMenu(menu: MenuGetModel) {
+    // Implementa esta función si necesitas eliminar menús
+  }
 
-  openModalDeleteMenu(menu: MenuGetModel){
-
-  };
-
-  openModalCreateMenu(){
+  openModalCreateMenu() {
     let RefDialog = this.dialog.open(ModalMenusComponent, {
       minWidth: '800px',
       minHeight: '80%',
       maxWidth: '50%',
       data: {
         title: TitlesModal.Menu,
-        question: "",
+        question: '',
         iconClass: Icons.Menu,
         type: TypeModal.Menu,
-        imageUser: "",
-        userName: ""
+        imageUser: '',
+        userName: ''
       }
-  });
-
-  
-
-
-
-}
-
+    });
+  }
 }
