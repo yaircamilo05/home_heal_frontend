080e5b9b7219e9db63ec8c3b8178ed4386645a2a
julianagomez21 <jgomezgal25@gmail.com>
2023-11-12 22:27:50 -0500

    Update init component HTML and SCSS

--- src/app/modules/website/pages/init/init.component.ts
+++ src/app/modules/website/pages/init/init.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-init',
   templateUrl: './init.component.html',
-  styleUrls: ['./init.component.scss']
+  styleUrls: ['./init.component.scss'],
 })
 export class InitComponent {
+
 
 }
