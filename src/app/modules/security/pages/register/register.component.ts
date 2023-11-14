e6827f3b8a749078b84572b0463f9186e75b4bc9
juandzapata <juan.zapata22871@ucaldas.edu.co>
2023-11-14 00:59:39 -0500

    Add image upload functionality

--- src/app/modules/security/pages/register/register.component.ts
+++ src/app/modules/security/pages/register/register.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Messages } from 'src/app/common/messages.const';
+import { FileModel } from 'src/app/models/file.model';
 import { PatientRegister } from 'src/app/models/patient.model';
+import { FileService } from 'src/app/services/file.service';
 import { ModalService } from 'src/app/services/modal.service';
 import { PatientService } from 'src/app/services/patient.service';
 
@@ -14,12 +17,16 @@ export class RegisterComponent {
   form_patient: FormGroup = new FormGroup('');
   form_familiar: FormGroup = new FormGroup('');
   patient: PatientRegister | null = null;
+  image_url: File | null = null;
   showPassword: boolean = false;
+  selected_file: boolean = false;
+  url_image: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private patientService: PatientService,
     private modalService: ModalService,
+    private fileService: FileService,
     private router: Router
     ) { 
     
@@ -28,43 +35,69 @@ export class RegisterComponent {
 
 
   register() {
-    if(this.form_patient.valid && this.form_familiar.valid) {
-      let data: PatientRegister = {
-        name: this.form_patient.value.name,
-        lastname: this.form_patient.value.lastname,
-        phone: this.form_patient.value.phone,
-        email: this.form_patient.value.email,
-        cc: this.form_patient.value.CC,
-        password: this.form_patient.value.password,
-        gender: this.form_patient.value.gender,
-        image_url: '',
-        birthdate: this.form_patient.value.birthdate,
-        address: this.form_patient.value.address,
-        description: this.form_patient.value.description,
-        familiar_name: this.form_familiar.value.familiar_name,
-        familiar_lastname: this.form_familiar.value.familiar_lastname,
-        familiar_email: this.form_familiar.value.familiar_email,
-        familiar_phone: this.form_familiar.value.familiar_phone,
-      }
-
-      console.log(data);
-      
-      this.patientService.register_patient(data).subscribe({
-        next: (response) => {
-          if(response) {
-            this.modalService.openModalConfirmationAction();
-            this.router.navigate(['/login']);
-            console.log(response);
+      if(this.form_patient.valid && this.form_familiar.valid) {
+        if(this.selected_file){
+          let file: FileModel = {
+            name: 'file',
+            file: this.image_url as File,
+            fileName: this.image_url?.name as string
           }
-        },
-        error: (error) => {
-          console.log(error);
+          this.fileService.upload_file(file).subscribe({
+            next: (response) => {
+              if(response) {
+                console.log(response);
+                this.url_image = response.toString();
+                this.register_patient();
+              }
+            },
+            error: (error) => {
+              console.log(error);
+              this.modalService.openToastErrorAction(Messages.ImageError);
+            }
+          })
+        }else{
+          this.register_patient();
         }
-      })
-      
     }
   }
 
+  register_patient(){
+    let data: PatientRegister = {
+      name: this.form_patient.value.name,
+      lastname: this.form_patient.value.lastname,
+      phone: this.form_patient.value.phone,
+      email: this.form_patient.value.email,
+      cc: this.form_patient.value.CC,
+      password: this.form_patient.value.password,
+      gender: this.form_patient.value.gender,
+      image_url: this.url_image,
+      birthdate: this.form_patient.value.birthdate,
+      address: this.form_patient.value.address,
+      description: this.form_patient.value.description,
+      familiar_name: this.form_familiar.value.familiar_name,
+      familiar_lastname: this.form_familiar.value.familiar_lastname,
+      familiar_email: this.form_familiar.value.familiar_email,
+      familiar_phone: this.form_familiar.value.familiar_phone,
+    }
+
+    console.log(data);
+    
+    this.patientService.register_patient(data).subscribe({
+      next: (response) => {
+        if(response) {
+          this.modalService.openToastSuccessAction(Messages.SuccessRegister);
+          this.router.navigate(['/login']);
+          console.log(response);
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        if (error.status == 500) {
+          this.modalService.openToastErrorAction(Messages.ErrorRegister);
+        }
+      }
+    })
+  }
 
   builForm() {
     this.form_patient = this.formBuilder.group({
@@ -73,11 +106,12 @@ export class RegisterComponent {
       phone: ['3104572663', [Validators.required]],
       address: ['Cra 8c#41-19', [Validators.required]],
       CC: ['1002633638', [Validators.required]],
-      description: ['Me gusta el pipi', [Validators.required]],
+      description: ['Me gusta el pipi'],
       gender: ['M', [Validators.required]],
       birthdate: ['2002-05-16', [Validators.required]],
       email: ['juanzapata_12@gmail.com', [Validators.required, Validators.email]],
-      password: ['Hola123*', Validators.required]
+      password: ['Hola123*', Validators.required],
+      image_url: ['']
     })
     
     this.form_familiar = this.formBuilder.group({
@@ -99,5 +133,11 @@ export class RegisterComponent {
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
 
+  }
+
+  onFileSelected(event: any){
+    this.image_url = event.target.files[0] as File;
+    this.selected_file = true;
+    console.log(this.image_url);
   }
 }
