f3e6e702b132f55d78548520753081ca50164d74
julianagomez21 <jgomezgal25@gmail.com>
2023-11-16 15:15:26 -0500

    Update patient model and registration form

--- src/app/modules/security/pages/register/register.component.ts
+++ src/app/modules/security/pages/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent {
   form_patient: FormGroup = new FormGroup('');
   form_familiar: FormGroup = new FormGroup('');
   patient: PatientRegister | null = null;
-  image_url: File | null = null;
+  image_file: File | null = null;
   showPassword: boolean = false;
   selected_file: boolean = false;
   url_image: string = '';
@@ -26,63 +26,40 @@ export class RegisterComponent {
     private formBuilder: FormBuilder,
     private patientService: PatientService,
     private modalService: ModalService,
-    private fileService: FileService,
     private router: Router
     ) { 
     
-    this.builForm();
+    this.buildForm();
   }
-
 
   register() {
-      if(this.form_patient.valid && this.form_familiar.valid) {
-        if(this.selected_file){
-          let file: FileModel = {
-            name: 'file',
-            file: this.image_url as File,
-            fileName: this.image_url?.name as string
-          }
-          this.fileService.upload_file(file).subscribe({
-            next: (response) => {
-              if(response) {
-                console.log(response);
-                this.url_image = response.toString();
-                this.register_patient();
-              }
-            },
-            error: (error) => {
-              console.log(error);
-              this.modalService.openToastErrorAction(Messages.ImageError);
-            }
-          })
-        }else{
-          this.register_patient();
-        }
-    }
-  }
 
-  register_patient(){
-    let data: PatientRegister = {
-      name: this.form_patient.value.name,
-      lastname: this.form_patient.value.lastname,
-      phone: this.form_patient.value.phone,
-      email: this.form_patient.value.email,
-      cc: this.form_patient.value.CC,
-      password: this.form_patient.value.password,
-      gender: this.form_patient.value.gender,
-      image_url: this.url_image,
-      birthdate: this.form_patient.value.birthdate,
-      address: this.form_patient.value.address,
-      description: this.form_patient.value.description,
-      familiar_name: this.form_familiar.value.familiar_name,
-      familiar_lastname: this.form_familiar.value.familiar_lastname,
-      familiar_email: this.form_familiar.value.familiar_email,
-      familiar_phone: this.form_familiar.value.familiar_phone,
+    const formData = new FormData();
+    formData.append('name', this.form_patient.value.name);
+    formData.append('lastname', this.form_patient.value.lastname);
+    formData.append('phone', this.form_patient.value.phone);
+    formData.append('email', this.form_patient.value.email);
+    formData.append('cc', this.form_patient.value.CC);
+    formData.append('password', this.form_patient.value.password);
+    formData.append('birthdate', this.form_patient.value.birthdate);
+    formData.append('address', this.form_patient.value.address);
+    formData.append('description', this.form_patient.value.description);
+    formData.append('gender', this.form_patient.value.gender);
+
+    if(this.selected_file){
+      console.log('entre');
+      
+      formData.append('image_file', this.image_file as File);
     }
 
-    console.log(data);
+    formData.append('familiar_name', this.form_familiar.value.familiar_name);
+    formData.append('familiar_lastname', this.form_familiar.value.familiar_lastname);
+    formData.append('familiar_email', this.form_familiar.value.familiar_email);
+    formData.append('familiar_phone', this.form_familiar.value.familiar_phone);
+
+    console.log(formData.get('image_file'));
     
-    this.patientService.register_patient(data).subscribe({
+    this.patientService.register_patient(formData).subscribe({
       next: (response) => {
         if(response) {
           this.modalService.openToastSuccessAction(Messages.SuccessRegister);
@@ -99,7 +76,7 @@ export class RegisterComponent {
     })
   }
 
-  builForm() {
+  buildForm() {
     this.form_patient = this.formBuilder.group({
       name: ['Juan', [Validators.required]],
       lastname: ['Zapata', Validators.required],
@@ -110,8 +87,7 @@ export class RegisterComponent {
       gender: ['M', [Validators.required]],
       birthdate: ['2002-05-16', [Validators.required]],
       email: ['juanzapata_12@gmail.com', [Validators.required, Validators.email]],
-      password: ['Hola123*', Validators.required],
-      image_url: ['']
+      password: ['Hola123*', Validators.required]
     })
     
     this.form_familiar = this.formBuilder.group({
@@ -136,8 +112,11 @@ export class RegisterComponent {
   }
 
   onFileSelected(event: any){
-    this.image_url = event.target.files[0] as File;
+    this.image_file = event.target.files[0] as File;
     this.selected_file = true;
-    console.log(this.image_url);
+    console.log(this.image_file);
+    // this.form_patient.patchValue({
+    //   image_file: this.image_file
+    // })
   }
 }
