72a7f29c3e73248ff3b2ff53b1ee7c767f7b68b3
juandzapata <juan.zapata22871@ucaldas.edu.co>
2023-11-22 23:47:33 -0500

    Functionality for patient cards and adjustments to the card in the init component.

--- src/app/modules/cms/pages/patient/patient.component.ts
+++ src/app/modules/cms/pages/patient/patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PatientService } from 'src/app/services/patient.service';
 
 @Component({
   selector: 'app-patient',
@@ -6,10 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient.component.scss']
 })
 export class PatientComponent {
-  title = 'Juan David Zapata López';
-  text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas.';
-  description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas.';
-  imageUrl = 'C:\\Users\\juanz\\OneDrive\\Escritorio\\General\\hvimage.png'  
-  constructor() { }
+  patients: any[] = [];
+  patient_user: any = {};
+  age: number = 0;
 
+  constructor(
+    private patientService: PatientService,
+  ) {}
+  
+  ngOnInit() {
+    this.getAllPatients();
+  }
+
+  getAllPatients() {
+    this.patientService.getPatients().subscribe(
+      (response) => {
+        // Verifica si obtienes los datos correctamente
+        console.log(response);
+        if (response && response.data) {
+          this.patients = response.data;
+          this.calculateAges();
+        }
+      },
+      (error) => {
+        console.error('Error al obtener los pacientes:', error);
+      }
+    );
+  }
+
+  /**
+   * En espera del get_user por 'id' para la implementación
+   */
+  getPatientUser() {
+  }
+
+  /**
+   * Calculate the patient's age to make it easier to visualize.
+   */
+  calculateAges() {
+    this.patients.forEach((patient) => {
+      patient.birthdate = this.patientService.calculateAge(new Date(patient.birthdate));
+    });
+  }
 }
--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.local';
 import { PatientRegister } from '../models/patient.model';
 import { Observable } from 'rxjs';
+import { ResponseCustomModel } from '../models/response.custom.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,44 @@ export class PatientService {
   register_patient(patient: FormData) {
     return this.http.post(`${this.server}/register_user`, patient);
   }
+  
+   /**
+   * Method to obtain the patients in the database
+   * @returns List of patients
+   */
+   getPatients(): Observable<ResponseCustomModel<PatientRegister[]>> {
+    return this.http.get<ResponseCustomModel<PatientRegister[]>>(`${this.server}/get_all_patients`);
+  }
 
+  /**
+   * 
+   * @returns void
+   */
   get_default_image(): Observable<Blob> {
     return this.http.get('assets/images/userImageNotFound.png', { responseType: 'blob' });
+  }
+
+  /**
+   * Method to calculate the age of a patient
+   * @param birthdate 
+   * @returns 
+   */
+  calculateAge(birthdate: Date): number {
+    const actualDate = new Date();
+    const actualYear = actualDate.getFullYear();
+    const actualMounth = actualDate.getMonth() + 1;
+    const actualDay = actualDate.getDate();
+
+    const birthYear = birthdate.getFullYear();
+    const mounthYear = birthdate.getMonth() + 1;
+    const birthDay = birthdate.getDate();
+
+    let age = actualYear - birthYear;
+
+    if (actualMounth < mounthYear || (actualMounth === mounthYear && actualDay < birthDay)) {
+      age--;
+    }
+
+    return age;
   }
 }
